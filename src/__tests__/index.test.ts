559cee9e7c91f3044ed520cb9cbfb087d6009395
moroball14 <daiki.mr.0330@gmail.com>
2021-01-05 19:14:19 +0900

    fix test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,26 +1,15 @@
 import { Counter } from "../index";
 
-jest.mock("../index");
-jest.spyOn(console, "log")
-
-const CounterMock = Counter as jest.Mock;
-
 test("count 1 to 100", () => {
-  const arr1_100 = Array.from(new Array(100).keys()).map((i)=> i + 1);
-  
-  CounterMock.mockImplementationOnce(() => {
-    return {
-      oneToHundred(): number[]{
-        console.log('hello world');
-        return arr1_100;
-      }
-    }
+  const spyLog = jest.spyOn(console, "log");
+
+  new Counter().oneToHundred();
+
+  expect(spyLog.mock.calls).toHaveLength(100);
+  Array.from(Array(100).keys()).forEach((i) => {
+    expect(spyLog.mock.calls[i]?.[0]).toBe(i + 1);
   });
 
-  const counter = new CounterMock();
-  const return_ary = counter.oneToHundred();
-
-  expect(CounterMock).toHaveBeenCalled();
-  expect(console.log).toBeCalled();
-  expect(return_ary).toEqual(arr1_100);
+  spyLog.mockReset();
+  spyLog.mockRestore();
 });
