ceac2f01a6f780a3f60bf947bd303f8086a88e18
moroball14 <daiki.mr.0330@gmail.com>
2021-01-05 15:47:55 +0900

    fix matcher for array

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,5 +3,5 @@ import { Counter } from '../index';
 test('count 1 to 100', () => {
   const arr1_100 = Array.from(new Array(100)).map((v,i)=> i + 1);
   const counter = new Counter()
-  expect(JSON.stringify(counter.oneToHundred())).toBe(JSON.stringify(arr1_100));
+  expect(counter.oneToHundred()).toEqual(arr1_100);
 });
