bbd3b44726caa40c6608525aaec309c91feae7d3
moroball14 <daiki.mr.0330@gmail.com>
2021-01-05 13:42:08 +0900

    add test file

---
+++ __tests__/index.test.ts
@@ -0,0 +1,6 @@
+import { Counter } from '../src/index';
+
+test('test', () => {
+  const arr1_100 = Array.from(new Array(100)).map((v,i)=> i + 1);
+  expect(new Counter().oneToHundred()).toBe(arr1_100);
+});
