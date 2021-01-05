dbbdc55c319448332f9c815262acdac8a9bbbb20
moroball14 <daiki.mr.0330@gmail.com>
2021-01-05 16:37:47 +0900

    mock class

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,25 @@
-import { Counter } from '../index';
+import { Counter } from "../index";
 
-test('count 1 to 100', () => {
+jest.mock("../index");
+jest.spyOn(console, "log")
+
+const CounterMock = Counter as jest.Mock;
+
+test("count 1 to 100", () => {
   const arr1_100 = Array.from(new Array(100).keys()).map((i)=> i + 1);
-  const counter = new Counter()
-  expect(counter.oneToHundred()).toEqual(arr1_100);
+  const counter = new Counter();
+  const return_ary = counter.oneToHundred();
+
+  CounterMock.mockImplementationOnce(() => {
+    return {
+      oneToHundred(): number[]{
+        console.log('hello world');
+        return arr1_100;
+      }
+    }
+  });
+
+  expect(CounterMock).toHaveBeenCalled();
+  expect(console.log).toBeCalled();
+  expect(return_ary).toEqual(arr1_100);
 });
