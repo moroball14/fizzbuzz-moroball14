e198206a45bc4c5fc20c3cb03ed7ac6ca8f4e7dc
moroball14 <daiki.mr.0330@gmail.com>
2021-01-05 16:01:07 +0900

    fix coding create array

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { Counter } from '../index';
 
 test('count 1 to 100', () => {
-  const arr1_100 = Array.from(new Array(100)).map((v,i)=> i + 1);
+  const arr1_100 = Array.from(new Array(100).keys()).map((i)=> i + 1);
   const counter = new Counter()
   expect(counter.oneToHundred()).toEqual(arr1_100);
 });
