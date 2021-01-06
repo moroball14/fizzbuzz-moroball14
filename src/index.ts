d79fe4e3ac61a4b311ebaa6a52e0e93819edf4df
moroball14 <daiki.mr.0330@gmail.com>
2021-01-06 16:05:17 +0900

    fix 15

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export class Counter {
   outputFizzBuzz(): void {
     for (let i = 1; i <= 100; i++) {
-      if (i % 15 === 0) {
+      if (i % (3 * 5) === 0) {
         console.log("Fizz Buzz");
       } else if (i % 3 === 0) {
         console.log("Fizz");
