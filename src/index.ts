0e3c3395bc8f4ac58242785075e5ec74bcf466ac
moroball14 <daiki.mr.0330@gmail.com>
2021-01-06 16:25:50 +0900

    refactor return fizzbuzz

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 export class Counter {
-  outputFizzBuzz(): void {
+  outputOneToHundred(): void {
     for (let i = 1; i <= 100; i++) {
-      if (i % (3 * 5) === 0) {
-        console.log("Fizz Buzz");
-      } else if (i % 3 === 0) {
-        console.log("Fizz");
-      } else if (i % 5 === 0) {
-        console.log("Buzz");
-      } else {
-        console.log(i);
-      }
+      console.log(this.returnFizzBuzz(i));
     };
+  };
+
+  returnFizzBuzz(number: number): any {
+    let result;
+    if (number % (3 * 5) === 0) {
+      result = "Fizz Buzz";
+    } else if (number % 3 === 0) {
+      result = "Fizz";
+    } else if (number % 5 === 0) {
+      result = "Buzz";
+    } else {
+      result = number;
+    };
+    return result;
   };
 };
 
 let counter = new Counter();
 
-counter.outputFizzBuzz();
+counter.outputOneToHundred();
