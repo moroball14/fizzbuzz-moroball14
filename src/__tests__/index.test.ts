19e8b40a1d53b740d958772ba383abf0eb13a839
moroball14 <daiki.mr.0330@gmail.com>
2021-01-07 12:38:15 +0900

    fix test case

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -7,24 +7,27 @@ describe("Counter class test", () => {
 
   test("output 1 to 100", () => {
     const spyLog = jest.spyOn(console, "log");
-    new Counter().outputOneToHundred();
+    const counter = new Counter()
+    let fizzbuzz = '';
+    for (let i = 1; i <= 100; i++) {
+      fizzbuzz += `${counter.returnFizzBuzz(i)},`;
+    };
+    counter.outputOneToHundred();
     const mockArray = spyLog.mock.calls;
+
     expect(mockArray).toHaveLength(1);
+    expect(typeof mockArray[0][0]).toBe("string");
+    expect(mockArray[0][0]).toBe(fizzbuzz.slice(0, -1));
   });
 
   test("return fizzbuzz", () => {
     const counter = new Counter;
     const num = 1;
-    for (let i = 1; i <= 100; i++) {
-      if (i % 15 === 0) {
-        expect(counter.returnFizzBuzz(num * i)).toBe('Fizz Buzz');
-      } else if (i % 3 === 0) {
-        expect(counter.returnFizzBuzz(num * i)).toBe('Fizz');
-      } else if (i % 5 === 0) {
-        expect(counter.returnFizzBuzz(num * i)).toBe('Buzz');
-      } else {
-        expect(counter.returnFizzBuzz(num)).toBe(num.toString());
-      };
+    for (let i = 1; i <= 15; i++) {
+      expect(counter.returnFizzBuzz(15 * i)).toBe('Fizz Buzz');
+      if (i % 15 !== 0 && i % 5 !== 0) expect(counter.returnFizzBuzz(3 * i)).toBe('Fizz');
+      if (i % 15 !== 0 && i % 3 !== 0) expect(counter.returnFizzBuzz(5 * i)).toBe('Buzz');
+      if (i % 15 !== 0 && i % 3 !== 0 && i % 5 !== 0) expect(counter.returnFizzBuzz(i)).toBe(i.toString());
     };
   });
 });
