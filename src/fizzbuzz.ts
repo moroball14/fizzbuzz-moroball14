14565c93ac05be1c64c96c233c247ebeb4122384
moroball14 <daiki.mr.0330@gmail.com>
2021-01-11 17:21:54 +0900

    identifier method

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,22 +1,22 @@
 export class FizzBuzz {
-  outputValue(): void {
+  public outputValue(): void {
     const numbers1_100: Array<number> = Array.from(new Array(100).keys()).map((i)=> i + 1);
     const fizzbuzz: Array<string> = this.conversion(numbers1_100);
     console.log(this.formatValue(fizzbuzz));
   };
 
-  conversion(numbers: Array<number>): Array<string> {
+  private conversion(numbers: Array<number>): Array<string> {
     return numbers.map(num => this.returnValue(num));
   }
 
-  returnValue(number: number): string {
+  private returnValue(number: number): string {
     if (number % 15 === 0) return "Fizz Buzz";
     if (number % 3 === 0) return "Fizz";
     if (number % 5 === 0) return "Buzz";
     return number.toString();
   };
 
-  formatValue(results: Array<string>): string {
+  private formatValue(results: Array<string>): string {
     return results.join(",");
   }
 };
